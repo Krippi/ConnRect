c6b43117bd0815c1ccde3ad1316bfe85cdda13dd
fubi927 <fubi927@gmail.com>
2022-11-30 20:22:43 +0100

    teständerung

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Du bist scheiße");
         System.out.println("Du auch!");
+        System.out.println("Test");
     }
 }
