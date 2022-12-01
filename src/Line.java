400686f53424625f882d1d0cb3ede43fbc4c94ff
fubi927 <fubi927@gmail.com>
2022-12-01 21:54:57 +0100

    enum linetypes

--- src/Line.java
+++ src/Line.java
@@ -2,7 +2,11 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class Line {
-    private String linetype = "straight";
+    public static enum linetypes{
+        STRAIGHT,
+        ANGLED,
+    };
+    private linetypes linetype = linetypes.STRAIGHT;
     private ArrayList<Linepart> linepartList = new ArrayList<>();
 
     public Line(Rectangle r1, Rectangle r2){
@@ -11,13 +15,13 @@ public class Line {
         Point end;
 
         switch (linetype){
-            case "straight":
+            case STRAIGHT:
                 start = r1.getMidPoint();
                 end = r2.getMidPoint();
 
                 linepartList.add(new Linepart(start, end));
                 break;
-            case "angled":
+            case ANGLED:
                 //First Line
 
                 break;
